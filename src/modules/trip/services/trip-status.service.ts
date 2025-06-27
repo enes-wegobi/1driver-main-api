3fb207880453bc48076432a5a8a4871d97fcba1d
enojen <enesozel.eo@gmail.com>
2025-06-27 15:39:13 +0200

    fix

--- src/modules/trip/services/trip-status.service.ts
+++ src/modules/trip/services/trip-status.service.ts
@@ -24,7 +24,7 @@ export class TripStatusService {
       ],
     },
     [TripStatus.DRIVER_NOT_FOUND]: {
-      to: [TripStatus.WAITING_FOR_DRIVER, TripStatus.CANCELLED],
+      to: [TripStatus.WAITING_FOR_DRIVER, TripStatus.CANCELLED, TripStatus.DRAFT],
     },
     [TripStatus.APPROVED]: {
       to: [TripStatus.DRIVER_ON_WAY_TO_PICKUP, TripStatus.CANCELLED],
