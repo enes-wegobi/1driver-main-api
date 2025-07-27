44840737bb1d9d7906d7144168fb479645e48716
enojen <enesozel.eo@gmail.com>
2025-07-27 22:40:52 +0200

    fix

--- src/modules/customers/customers.module.ts
+++ src/modules/customers/customers.module.ts
@@ -4,12 +4,16 @@ import { CustomersController } from './customers.controller';
 import { CustomersService } from './customers.service';
 import { S3Module } from 'src/s3/s3.module';
 import { JwtModule } from 'src/jwt/jwt.module';
+import { SupportTicketsModule } from '../support-tickets/support-tickets.module';
+import { RedisModule } from 'src/redis/redis.module';
 
 @Module({
   imports: [
     ClientsModule,
     S3Module,
     JwtModule,
+    SupportTicketsModule,
+    RedisModule,
   ],
   controllers: [CustomersController],
   providers: [CustomersService],
