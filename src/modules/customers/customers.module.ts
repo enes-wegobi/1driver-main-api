4d2a6d7c09c9e3230c1254d890d22d1f78dc13d3
enojen <enesozel.eo@gmail.com>
2025-07-27 22:39:14 +0200

    fix

--- src/modules/customers/customers.module.ts
+++ src/modules/customers/customers.module.ts
@@ -3,14 +3,12 @@ import { ClientsModule } from '../../clients/clients.module';
 import { CustomersController } from './customers.controller';
 import { CustomersService } from './customers.service';
 import { S3Module } from 'src/s3/s3.module';
-import { PaymentsModule } from '../payments/payments.module';
 import { JwtModule } from 'src/jwt/jwt.module';
 
 @Module({
   imports: [
     ClientsModule,
     S3Module,
-    PaymentsModule,
     JwtModule,
   ],
   controllers: [CustomersController],
