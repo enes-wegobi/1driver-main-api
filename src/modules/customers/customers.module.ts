9b944258777f55319fee960717340fbf2b6af93e
enojen <enesozel.eo@gmail.com>
2025-07-27 22:33:26 +0200

    fix

--- src/modules/customers/customers.module.ts
+++ src/modules/customers/customers.module.ts
@@ -6,6 +6,7 @@ import { RedisModule } from 'src/redis/redis.module';
 import { S3Module } from 'src/s3/s3.module';
 import { SupportTicketsModule } from 'src/modules/support-tickets/support-tickets.module';
 import { JwtModule } from 'src/jwt/jwt.module';
+import { PaymentsModule } from '../payments/payments.module';
 
 @Module({
   imports: [
@@ -14,6 +15,7 @@ import { JwtModule } from 'src/jwt/jwt.module';
     RedisModule,
     S3Module,
     SupportTicketsModule,
+    PaymentsModule,
   ],
   controllers: [CustomersController],
   providers: [CustomersService],
